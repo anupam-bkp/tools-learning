58b838757cfe1b50f935b941c38d0d569652c367
anupam <NG8E391@eu.airbus.corp>
2020-09-06 06:38:59 +0530

    User4 committing changes to UserBBB

--- GitProject1/src/com/example/user2/UserBBB.java
+++ GitProject1/src/com/example/user2/UserBBB.java
@@ -6,6 +6,7 @@ public class UserBBB {
 	public static void main(String[] args) {
 		System.out.println("UserBBB");
 		System.out.println("Hello UserBBB");
+		System.out.println("Again Hello UserBBB");
 	}
 
 }
